56decd2f3e22bb740043d752c0cfa3d6a5dceaa7
Zink Rau <Zink Rau@DESKTOP-281ALGC>
2019-04-08 19:45:02 +0700

    Chinh sua trang khoa hoc

--- SmallSchool/Controllers/CoursesController.cs
+++ SmallSchool/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmallSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,11 @@ namespace SmallSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        public readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }   
         // GET: Courses
         public ActionResult Create ()
         {
