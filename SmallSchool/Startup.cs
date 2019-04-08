0a62c9785ecc930be6c0a3a2889d3d3cb90e27b4
Zink Rau <Zink Rau@DESKTOP-281ALGC>
2019-04-08 18:19:37 +0700

    Add project files.

---
+++ SmallSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmallSchool.Startup))]
+namespace SmallSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
